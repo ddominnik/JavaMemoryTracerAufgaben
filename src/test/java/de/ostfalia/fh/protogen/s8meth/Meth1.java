a0aae5320737e2286f2ad63d74426e178e9d92cf
Unknown <dominiktews98@gmail.com>
2018-04-10 09:43:51 +0200

    Meth1 und TestMeth1 überarbeitet

--- src/test/java/de/ostfalia/fh/protogen/s8meth/Meth1.java
+++ src/test/java/de/ostfalia/fh/protogen/s8meth/Meth1.java
@@ -2,7 +2,7 @@ package test.java.de.ostfalia.fh.protogen.s8meth;
 
 public class Meth1 {
 	public static void main(String[] args) {
-		Meth1A a = new Meth1A();
+		Meth1A a = new Meth1A(1,1);
 		a.x = 23;
 		a.y = 19;
 		int sum = a.add();
@@ -12,6 +12,11 @@ public class Meth1 {
 class Meth1A {
 	int x, y;
 
+	Meth1A (int x, int y) {
+		this.x = x; 
+		this.y = y;
+	}
+	
 	int add() {
 		int z = x + y;
 		return z;
