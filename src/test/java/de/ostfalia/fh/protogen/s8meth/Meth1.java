ab39fcf5b579c46d1d2259de26d6970ece39162e
Unknown <dominiktews98@gmail.com>
2018-04-10 09:19:41 +0200

    Rename "A" to "Meth1A"

--- src/test/java/de/ostfalia/fh/protogen/s8meth/Meth1.java
+++ src/test/java/de/ostfalia/fh/protogen/s8meth/Meth1.java
@@ -2,14 +2,14 @@ package test.java.de.ostfalia.fh.protogen.s8meth;
 
 public class Meth1 {
 	public static void main(String[] args) {
-		A a = new A();
+		Meth1A a = new Meth1A();
 		a.x = 23;
 		a.y = 19;
 		int sum = a.add();
 	}
 }
 
-class A {
+class Meth1A {
 	int x, y;
 
 	int add() {
