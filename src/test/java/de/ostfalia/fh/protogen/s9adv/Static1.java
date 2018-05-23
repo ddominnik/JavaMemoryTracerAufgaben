539959145ab821c34211aa43727032062728791f
Unknown <dominiktews98@gmail.com>
2018-05-23 11:56:00 +0200

    Static1 WIP

--- src/test/java/de/ostfalia/fh/protogen/s9adv/Static1.java
+++ src/test/java/de/ostfalia/fh/protogen/s9adv/Static1.java
@@ -1,30 +1,28 @@
 package test.java.de.ostfalia.fh.protogen.s9adv;
 
 public class Static1 {
+static int stat = 0;
+	
+	public Static1(int i) {
+		super(); stat = i;
+	}
+	
+	int inc (int i) {
+		stat = stat + i;
+		return stat;
+	}
+	
 	public static void main(String[] args) {
-		Static1A a = new Static1A(0);
+		Static1 a = new Static1(0);
 		int x = a.inc(2);
-		Static1A aclone = a;
+		Static1 aclone = a;
 		int y = a.inc(2);
-		Static1A a2 = new Static1A(1);
+		Static1 a2 = new Static1(1);
 		int z = a2.inc(5);
 		if (a.stat != a2.stat) {
 			a.inc(1);
 			a2.inc(1);
 		}
-	}
-}
-
-class Static1A {
-	static int stat = 0;
-	
-	public Static1A(int i) {
-		super(); stat = i;
-	}
-	
-	int inc (int i) {
-		stat += i;
-		return stat;
 	}
 }
 
--- src/test/java/de/ostfalia/fh/protogen/s9adv/TestStatic1.java
+++ src/test/java/de/ostfalia/fh/protogen/s9adv/TestStatic1.java
@@ -3,61 +3,43 @@ package test.java.de.ostfalia.fh.protogen.s9adv;
 import org.junit.Test;
 import de.ostfalia.fh.protogen.TestBase2;
 
-public class TestStatic1 extends TestBase2{
+public class TestStatic1 extends TestBase2 {
 
-	String[][] stack = {
-			{"5", "this=1x0","i=0"},
-			{"22","this=1x0","i=0"},
-			{"5","a=1x0"},
-			{"6","a=1x0","ret=?","this=1x0","i=2"},
-			{"26","a=1x0","ret=?","this=1x0","i=2"},
-			{"27","a=1x0","ret=2","this=1x0","i=2"},
-			{"6","a=1x0","x=2"},
-			{"7","a=1x0","x=2","aclone=1x0"},
-			{"8","a=1x0","x=2","aclone=1x0","ret=?","this=1x0","i=2"},
-			{"26","a=1x0","x=2","aclone=1x0","ret=?","this=1x0","i=2"},
-			{"27","a=1x0","x=2","aclone=1x0","ret=4","this=1x0","i=2"},
-			{"8","a=1x0","x=2","aclone=1x0","y=4"},
-			{"9","a=1x0","x=2","aclone=1x0","y=4","this=1x1","i=1"},
-			{"22","a=1x0","x=2","aclone=1x0","y=4","this=1x1","i=1"},
-			{"9","a=1x0","x=2","aclone=1x0","y=4","a2=1x1"},
-			{"10","a=1x0","x=2","aclone=1x0","y=4","a2=1x1","ret=?","this=1x1","i=5"},
-			{"26","a=1x0","x=2","aclone=1x0","y=4","a2=1x1","ret=?","this=1x1","i=5"},
-			{"27","a=1x0","x=2","aclone=1x0","y=4","a2=1x1","ret=6","this=1x1","i=5"},
-			{"10","a=1x0","x=2","aclone=1x0","y=4","a2=1x1","z=6"},
-			{"11","a=1x0","x=2","aclone=1x0","y=4","a2=1x1","z=6"},
-	};
-	
-	String[][] heap = {
-			{"5"},{"22"},{"5"},{"6"},{"26"},{"27"},{"6"},{"7"},{"8"},{"26"},{"27"},{"8"},
-			{"9"},{"22"},{"9"},{"10"},{"26"},{"27"},{"10"},{"11"}
-	};
-	
-	String[][] stat = {
-			{"5","stat=0"},
-			{"22","stat=0"},
-			{"5","stat=0"},
-			{"6","stat=0"},
-			{"26","stat=2"},
-			{"27","stat=2"},
-			{"6","stat=2"},
-			{"7","stat=2"},
-			{"8","stat=2"},
-			{"26","stat=4"},
-			{"27","stat=4"},
-			{"8","stat=4"},
-			{"9","stat=4"},
-			{"22","stat=1"},
-			{"9","stat=1"},
-			{"10","stat=1"},
-			{"26","stat=6"},
-			{"27","stat=6"},
-			{"10","stat=6"},
-			{"11","stat=6"}
-	};
-	
+	String[][] stack = { 
+			{ "16", "this=0x0", "i=0" }, 
+			{ "7", "this=0x0", "i=0" }, 
+			{ "16", "a=0x0" },
+			{ "17", "a=0x0", "ret=?", "this=0x0", "i=2" }, 
+			{ "11", "a=0x0", "ret=?", "this=0x0", "i=2" },
+			{ "12", "a=0x0", "ret=2", "this=0x0", "i=2" }, 
+			{ "17", "a=0x0", "x=2" },
+			{ "18", "a=0x0", "x=2", "aclone=0x0" }, 
+			{ "19", "a=0x0", "x=2", "aclone=0x0", "ret=?", "this=0x0", "i=2" },
+			{ "11", "a=0x0", "x=2", "aclone=0x0", "ret=?", "this=0x0", "i=2" },
+			{ "12", "a=0x0", "x=2", "aclone=0x0", "ret=4", "this=0x0", "i=2" },
+			{ "19", "a=0x0", "x=2", "aclone=0x0", "y=4" },
+			{ "20", "a=0x0", "x=2", "aclone=0x0", "y=4", "this=0x1", "i=1" },
+			{ "7", "a=0x0", "x=2", "aclone=0x0", "y=4", "this=0x1", "i=1" },
+			{ "20", "a=0x0", "x=2", "aclone=0x0", "y=4", "a2=0x1" },
+			{ "21", "a=0x0", "x=2", "aclone=0x0", "y=4", "a2=0x1", "ret=?", "this=0x1", "i=5" },
+			{ "11", "a=0x0", "x=2", "aclone=0x0", "y=4", "a2=0x1", "ret=?", "this=0x1", "i=5" },
+			{ "12", "a=0x0", "x=2", "aclone=0x0", "y=4", "a2=0x1", "ret=6", "this=0x1", "i=5" },
+			{ "21", "a=0x0", "x=2", "aclone=0x0", "y=4", "a2=0x1", "z=6" },
+			{ "22", "a=0x0", "x=2", "aclone=0x0", "y=4", "a2=0x1", "z=6" } };
+
+	String[][] heap = { { "16" }, { "7" }, { "16" }, { "17" }, { "11" }, { "12" }, { "17" }, { "18" }, { "19" },
+			{ "11" }, { "12" }, { "19" }, { "20" }, { "7" }, { "20" }, { "21" }, { "11" }, { "12" }, { "21" },
+			{ "22" } };
+
+	String[][] stat = { 
+			{ "16", "stat=0" }, { "7", "stat=0" }, { "16", "stat=0" }, { "17", "stat=0" },
+			{ "11", "stat=2" }, { "12", "stat=2" }, { "17", "stat=2" }, { "18", "stat=2" }, { "19", "stat=2" },
+			{ "11", "stat=4" }, { "12", "stat=4" }, { "19", "stat=4" }, { "20", "stat=4" }, { "7", "stat=1" },
+			{ "20", "stat=1" }, { "21", "stat=1" }, { "11", "stat=6" }, { "12", "stat=6" }, { "21", "stat=6" },
+			{ "22", "stat=6" } };
+
 	@Test
-	public void test () {
+	public void test() {
 		runTest("s9adv/", "Static1", stack, heap, stat);
 	}
 }
