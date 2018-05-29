c259d0dd02c5e042ddc85cf2e5777093ed4f204c
Unknown <dominiktews98@gmail.com>
2018-05-29 16:26:10 +0200

    Static3 verkürzt und etwas vereinfacht

--- src/test/java/de/ostfalia/fh/protogen/s9adv/Static3.java
+++ src/test/java/de/ostfalia/fh/protogen/s9adv/Static3.java
@@ -2,26 +2,27 @@ package test.java.de.ostfalia.fh.protogen.s9adv;
 
 public class Static3 {
 	static int stat = 0;
-	
+
 	public Static3(int i) {
 		super(); stat = i;
 	}
-	
-	int inc (int i) {
-		stat += i;
-		return stat;
-	}
-	
+
 	public static void main(String[] args) {
 		Static3 a = new Static3(0);
-		int x = a.inc(2);
+		int x = Inc.f(a, 2);
 		Static3 aclone = a;
-		int y = a.inc(2);
+		int y = Inc.f(a, 2);
 		Static3 a2 = new Static3(1);
-		int z = a2.inc(5);
+		int z = Inc.f(a2, 3);
 		if (a.stat != a2.stat) {
-			a.inc(1);
-			a2.inc(1);
+			Inc.f(a, 1);
+			Inc.f(a2, 1);
 		}
+	}
+}
+
+class Inc {
+	static int f(Static3 o, int i) {
+		o.stat += i; return o.stat;
 	}
 }
