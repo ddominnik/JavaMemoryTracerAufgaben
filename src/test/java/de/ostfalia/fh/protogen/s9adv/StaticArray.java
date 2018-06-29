3d5ea33d2911a4b37e39122eef084c8c28e207e8
Unknown <dominiktews98@gmail.com>
2018-06-29 14:09:32 +0200

    StaticArray hinzugefügt (hängt sich auf)

---
+++ src/test/java/de/ostfalia/fh/protogen/s9adv/StaticArray.java
@@ -0,0 +1,21 @@
+package test.java.de.ostfalia.fh.protogen.s9adv;
+
+public class StaticArray {
+	static int[] a;
+
+	public static void main(String[] args) {
+		a = new int[2];
+		boolean t = false;
+		int[] b = { 8, 2, 1 };
+		for (int i = 0; i < b.length; i++) {
+			if (b[i] <= i && i % 2 == 0) {
+				a[i] = b[i] + i;
+				b[i] = i % 2;
+				a[i]++;
+			}
+		}
+		if (a[1] < a[0]) {
+			t = true;
+		}
+	}
+}
